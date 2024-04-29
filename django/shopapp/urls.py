01ad37912729261e5e6533add7819a323ec36bf6
Nikita Mavrinsky <sharabarovnv@gmail.com>
2024-04-29 15:02:10 +0600

    django_s3

--- django/shopapp/urls.py
+++ django/shopapp/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+from .views import (Clients, SpecificClient,
+                    ProductsForDays)
+
 urlpatterns = [
+    path('clients/', Clients.as_view(), name='clients'),
+    path('clients/<int:client_id>/', SpecificClient.as_view(), name='specific_client'),
+    path('products/<int:client_id>/<int:days>/', ProductsForDays.as_view(), name='products_for_days')
 ]
--- django/shopapp/views.py
+++ django/shopapp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
-# Create your views here.
+from datetime import date, timedelta
+
+from .models import Client, Order
+
+
+class Clients(View):
+    def get(self, request):
+        context = {'clients': Client.objects.all()}
+        return render(request, 'shopapp/clients.html', context)
+
+
+class SpecificClient(View):
+    def get(self, request, client_id):
+        client = get_object_or_404(Client, pk=client_id)
+        context = {'client': client}
+        return render(request, 'shopapp/specific_client.html', context)
+
+
+class ProductsForDays(View):
+    def get(self, request, client_id, days):
+        client = get_object_or_404(Client, pk=client_id)
+        target_date = date.today() - timedelta(days=days)
+        orders = Order.objects.filter(client=client, placing_date__gt=target_date)
+        print(len(orders))
+        products = set()
+        for order in orders:
+            products.update(order.products.all())
+        context = {
+            'client': client,
+            'days': days,
+            'products': products,
+        }
+        return render(request, 'shopapp/products_for_days.html', context)
